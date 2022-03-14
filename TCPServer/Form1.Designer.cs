23bd8f8fe3a832e0356883e05ef7d34863b2fe74
sekr1 <sekr1@352-001>
2022-03-14 11:31:14 +0300

    Добавьте файлы проекта.

---
+++ TCPServer/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace TCPServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStartServer = new System.Windows.Forms.Button();
+            this.buttonStopServer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonStartServer
+            // 
+            this.buttonStartServer.Location = new System.Drawing.Point(361, 12);
+            this.buttonStartServer.Name = "buttonStartServer";
+            this.buttonStartServer.Size = new System.Drawing.Size(104, 23);
+            this.buttonStartServer.TabIndex = 0;
+            this.buttonStartServer.Text = "Start server";
+            this.buttonStartServer.UseVisualStyleBackColor = true;
+            // 
+            // buttonStopServer
+            // 
+            this.buttonStopServer.Location = new System.Drawing.Point(361, 41);
+            this.buttonStopServer.Name = "buttonStopServer";
+            this.buttonStopServer.Size = new System.Drawing.Size(104, 23);
+            this.buttonStopServer.TabIndex = 1;
+            this.buttonStopServer.Text = "Stop server";
+            this.buttonStopServer.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 228);
+            this.Controls.Add(this.buttonStopServer);
+            this.Controls.Add(this.buttonStartServer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button buttonStartServer;
+        private Button buttonStopServer;
+    }
+}
---
+++ TCPServer/Form1.cs
@@ -0,0 +1,10 @@
+namespace TCPServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
